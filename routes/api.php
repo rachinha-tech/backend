40525075bc265f37534a3ece7b37fa91ffba6d6d
gabriell-silva <gabrielsilva.freires2@gmail.com>
2023-05-05 08:52:48 -0300

    [WIP] - Definindo rotas da aplicação

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,27 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ModalityController;
+use App\Http\Controllers\User\AuthController;
+use App\Http\Controllers\User\RegisterController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
-//Route::prefix('/auth')->group(function () {
-//    Route::post('/login', [AuthController::class, 'redirectToProvider']);
-//});
+Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/login', [AuthController::class, 'store']);
+Route::post('/logout', [AuthController::class, 'destroy']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('/user')->group(function () {
+        Route::get('/{id}', [UserController::class, 'show']);
+        Route::put('/{id}', [UserController::class, 'update']);
+        Route::delete('/{id}', [UserController::class, 'destroy']);
+        Route::patch('/level/{id}', [UserController::class, 'level']);
+    });
+
+    Route::prefix('/modalities')->group(function () {
+        Route::get('/', [ModalityController::class, 'index']);
+        Route::post('/', [ModalityController::class, 'store']);
+    });
+});
+
+
