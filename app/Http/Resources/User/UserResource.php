b8a33fa09bbb75278c002d5c0411ebb0d1c2c392
gabriell-silva <gabrielsilva.freires2@gmail.com>
2023-05-07 00:23:44 -0300

    [FTR] - Incluindo resource de user

---
+++ app/Http/Resources/User/UserResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\User;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "login" => $this->login,
+            "date_birth" => $this->date_birth,
+            "email" => $this->email,
+            "picture" => $this->picture,
+            "level" => $this->level,
+            "active" => $this->active,
+        ];
+    }
+}
