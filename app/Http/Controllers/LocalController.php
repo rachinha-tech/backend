23233c23b1387fd3ddcb0b9336024393e16f3b64
gabriell-silva <gabrielsilva.freires2@gmail.com>
2023-05-24 21:23:40 -0300

    [WIP] - Ajustes no local

---
+++ app/Http/Controllers/LocalController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Actions\Local\DestroyLocal;
+use App\Actions\Local\ListLocal;
+use App\Actions\Local\ShowLocal;
+use App\Actions\Local\StoreLocal;
+use App\Actions\Local\UpdateLocal;
+use App\Http\Requests\Local\StoreLocalRequest;
+use App\Http\Requests\Local\UpdateLocalRequest;
+use App\Traits\ApiResponse;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class LocalController extends Controller
+{
+    use ApiResponse;
+
+    public function index(ListLocal $listLocal): JsonResponse
+    {
+        try {
+            return $this->success('Locais listados com sucesso.', $listLocal->handle());
+        } catch (\Exception) {
+            return $this->error('Erro ao listar locais.', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    public function store(StoreLocalRequest $request, StoreLocal $storeLocal): JsonResponse
+    {
+        try {
+            $local = $storeLocal->handle($request->validated());
+            return $this->success('Local criado com sucesso.', $local);
+        } catch (\DomainException $domainException) {
+            return $this->error($domainException->getMessage(), $domainException->getCode());
+        } catch (\Exception $exception) {
+            return $this->error('Erro ao criar local.'.$exception, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function show(int $id, ShowLocal $showLocal)
+    {
+        try {
+            return $this->success('Local listado com sucesso.', $showLocal->handle($id));
+        } catch (\DomainException $domainException) {
+            return $this->error($domainException->getMessage(), $domainException->getCode());
+        } catch (\Exception) {
+            return $this->error('Erro ao listar local.', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function update(int $id, UpdateLocalRequest $request, UpdateLocal $updateLocal): JsonResponse
+    {
+        try {
+            $local = $updateLocal->handle($id, $request->validated());
+            return $this->success('Local atualizado com sucesso!', $local);
+        } catch (\DomainException $domainException) {
+            return $this->error($domainException->getMessage(), $domainException->getCode());
+        } catch (\Exception) {
+            return $this->error('Erro ao atualizar local.', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function destroy(int $id, DestroyLocal $destroyLocal): JsonResponse
+    {
+        try {
+            $destroyLocal->handle($id);
+            return $this->success('Local excluído com sucesso!', []);
+        } catch (\DomainException $domainException) {
+            return $this->error($domainException->getMessage(), $domainException->getCode());
+        } catch (\Exception) {
+            return $this->error('Erro ao excluir local.', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ app/Http/Resources/Local/LocalResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\Local;
+
+use App\Models\Convenience;
+use App\Models\Modality;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LocalResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "description" => $this->desciption,
+            "url_image" => $this->url_image,
+            "value_of_hour" => $this->value_of_hour,
+            "modality_id" => $this->modality_id,
+            "points_id" => $this->points_id,
+            "modality" => $this->modality,
+            "convenience" => $this->convenience,
+        ];
+    }
+}
