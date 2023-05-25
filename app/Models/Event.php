7994e8d38577fbf63339bdf8018a495982bafc6f
gabriell-silva <gabrielsilva.freires2@gmail.com>
2023-05-25 00:07:40 -0300

    [RFR] - Incluindo relacionamentos

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Event extends Model
@@ -12,4 +13,9 @@ class Event extends Model
     use SoftDeletes;
 
     protected $guarded = ['id'];
+
+    public function schedule(): HasOne
+    {
+        return $this->hasOne(Schedule::class, 'schedule_id', 'id');
+    }
 }
