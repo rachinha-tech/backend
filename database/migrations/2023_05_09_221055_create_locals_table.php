ef88bd0f351b4cb329887055d428d4cfc5dfe3ec
gabriell-silva <gabrielsilva.freires2@gmail.com>
2023-05-24 21:25:57 -0300

    [WIP] - Alterando migrate de local

--- database/migrations/2023_05_09_221055_create_locals_table.php
+++ database/migrations/2023_05_09_221055_create_locals_table.php
@@ -4,21 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('locals', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('points_id');
-            $table->unsignedBigInteger('shuedule_id');
             $table->unsignedBigInteger('modality_id');
             $table->string('name')->unique();
+            $table->integer('value_of_hour');
+            $table->float('latitude', 8, 5);
+            $table->float('longitude', 8, 5);
             $table->string('description')->nullable();
-            $table->string('image')->nullable();
+            $table->string('url_image')->nullable();
 
             $table->timestamps();
             $table->softDeletes();
+
+            $table->foreign('modality_id')->references('id')->on('modalities');
         });
     }
 
