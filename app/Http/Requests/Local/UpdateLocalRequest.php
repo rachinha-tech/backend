8bd151f40802a1bd28607e28ced01464209cc1f6
gabriell-silva <gabrielsilva.freires2@gmail.com>
2023-05-24 21:24:36 -0300

    [WIP] - Implementando atualização de dados do local

---
+++ app/Http/Requests/Local/UpdateLocalRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests\Local;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateLocalRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'string',
+            'description' => 'string',
+            'url_image' => 'string'
+        ];
+    }
+}
